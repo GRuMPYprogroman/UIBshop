a9629d3c6055f2aa123f5e10997ce77ad161642e
Степан Лазарев <grebdloz@mail.ru>
2026-05-17 22:30:49 +0300

    Added sqlLite and controllers

---
+++ BlazorApp1/BlazorApp1/Controllers/SpecialsController.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApp1.Data;
+using BlazorApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController(PizzaStoreContext _db) : Controller
+{
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return await _db.Specials
+                                    .OrderByDescending(s => s.BasePrice)
+                                    .ToListAsync();
+    }
+}
--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,10 +1,21 @@
+using System.Globalization;
 using BlazorApp1.Components;
+using BlazorApp1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var culture = CultureInfo.InvariantCulture;
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=PizzaStore.db");
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -19,10 +30,26 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
